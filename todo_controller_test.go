ff281ac32bb61ca10773399dcf8ff05aae090930
Ruslan Balkin <baron@bytexgames.ru>
2019-03-06 16:01:23 +0300

    Fixed deprecated stuff

--- todo_controller_test.go
+++ todo_controller_test.go
@@ -14,7 +14,7 @@ func TestListTodoWorks(t *testing.T) {
 	todo.ConnectToTestDatabase()
 	w := MakeGinRequest(router, "GET", "/api/v1/todo/item/")
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Contains(t, w.HeaderMap["Content-Type"], "application/json; charset=utf-8")
+	assert.Contains(t, w.Header().Get("Content-Type"), "application/json")
 
 	var items []todo.TodoItem
 	if err := json.Unmarshal(w.Body.Bytes(), &items); err != nil {
@@ -27,7 +27,7 @@ func TestCountTodoWorks(t *testing.T) {
 	todo.ConnectToTestDatabase()
 	w := MakeGinRequest(router, "GET", "/api/v1/todo/count")
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Contains(t, w.HeaderMap["Content-Type"], "application/json; charset=utf-8")
+	assert.Contains(t, w.Header().Get("Content-Type"), "application/json")
 
 	var countStruct controllers.CountStruct
 	if err := json.Unmarshal(w.Body.Bytes(), &countStruct); err != nil {
@@ -40,7 +40,7 @@ func TestCountAllTodoWorks(t *testing.T) {
 	todo.ConnectToTestDatabase()
 	w := MakeGinRequest(router, "GET", "/api/v1/todo/countall")
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Contains(t, w.HeaderMap["Content-Type"], "application/json; charset=utf-8")
+	assert.Contains(t, w.Header().Get("Content-Type"), "application/json")
 
 	var countStruct controllers.CountStruct
 	if err := json.Unmarshal(w.Body.Bytes(), &countStruct); err != nil {
